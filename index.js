d5cda0d606c9375a9af40de000032b75213215ac
metalfrenzy747 <allenjeremy747@gmail.com>
2024-01-08 12:52:29 -0500

    As if lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(depth){
+  if (depth <= 400) {
+    return `This one is on me!`;
+  } else {
+    if (depth <= 2000) {
+      return `That will be twenty bucks.`;
+    } else {
+      if (depth <= 2500) {
+        return `I will gladly take your thirty bucks.`;
+      } else {
+        return `No can do.`
+      }
+    }
+  }
+}
+function ternaryCheckCity(city){
+  if (city === `NYC`) {
+    return `Ok, sounds good.`
+  } else {
+    return `No go.`
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let reply;
+  switch (tip) {
+    case "generous":
+      reply = `Thank you so much.`
+      break;
+    case "not as generous":
+      reply = `Thank you.`;
+      break;
+    default:
+      reply = `Bye.`;
+  }
+  return reply
 }
